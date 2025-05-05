41437f371a737129c21d18df945a446637bfe79a
Nick Andriievskyi <nandrievsky@gmail.com>
2025-05-05 17:04:21 +0200

    clean up the logs

--- backend/loggers/logger.go
+++ backend/loggers/logger.go
@@ -8,12 +8,21 @@ import (
 
 func NewLogger(conf *config.Config) *slog.Logger {
 	level := slog.LevelInfo
+	var ra = func(groups []string, a slog.Attr) slog.Attr {
+		if a.Key == slog.TimeKey {
+			return slog.Attr{} // remove time
+		}
+		return a
+	}
 	if conf.EnableDebug {
 		level = slog.LevelDebug
+		ra = nil
 	}
+
 	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-		AddSource: true,
-		Level:     level,
+		AddSource:   conf.EnableDebug,
+		Level:       level,
+		ReplaceAttr: ra,
 	})
 
 	return slog.New(handler)
